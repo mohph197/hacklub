f5263a9de7ed65ddcce09f4ab7e30b737b7ee064
Mohammed Tirichine <mohtiri718@gmail.com>
2021-02-24 20:52:52 +0100

    updates

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function onDay(index)
     var daysNums = document.querySelectorAll(".day > .num");
     days[index].style.color="#5ffcad";
     daysContents[index].style.backgroundColor="#2d27fb60";
-    daysContents[index].style.height="200px";
+    daysContents[index].style.height="100%";
     daysContents[index].style.borderColor="#5ffcad";
     daysNums[index].style.backgroundColor="#2d27fb60";
     daysNums[index].style.borderColor="#5ffcad";
@@ -17,7 +17,7 @@ function offDay(index)
     var daysNums = document.querySelectorAll(".day > .num");
     days[index].style.color="white";
     daysContents[index].style.backgroundColor="transparent";
-    daysContents[index].style.height="0px";
+    daysContents[index].style.height="0%";
     daysContents[index].style.borderColor="white";
     daysNums[index].style.backgroundColor="transparent";
     daysNums[index].style.borderColor="white";
@@ -45,4 +45,33 @@ function offArticle(index)
     articleContent[index].style.borderLeft = "2px solid white";
     articleContent[index].style.borderBottom = "none";
     // articleContent[index].style.boxShadow = "2px 5px 10px rgb(107, 150, 190)";
+}
+var count = 0;
+function nextOne()
+{
+    var workshop = document.getElementsByClassName("workshop");
+    if(count < 2)
+    {
+        count++;
+        workshop[count - 1].style.height = "0px";
+        workshop[count - 1].style.width = "0px";
+        workshop[count - 1].style.opacity = "0";
+        workshop[count].style.height = "500px";
+        workshop[count].style.width = "400px";
+        workshop[count].style.opacity = "1";
+    }
+}
+function preOne()
+{
+    var workshop = document.getElementsByClassName("workshop");
+    if(count > 0)
+    {
+        workshop[count - 1].style.height = "500px";
+        workshop[count - 1].style.width = "400px";
+        workshop[count - 1].style.opacity = "1";
+        workshop[count].style.height = "0px";
+        workshop[count].style.width = "0px";
+        workshop[count].style.opacity = "0";
+        count--;
+    }
 }
