ce667eadfdf70be07414613a04db24ad97300236
Mohammed Tirichine <mohtiri718@gmail.com>
2021-02-24 23:13:17 +0100

    updates

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function onDay(index)
     var days = document.querySelectorAll(".day");
     var daysContents = document.querySelectorAll(".day > .content");
     var daysNums = document.querySelectorAll(".day > .num");
-    days[index].style.color="#5ffcad";
+    days[index].style.color="#5ffce7";
     daysContents[index].style.backgroundColor="#2d27fb60";
     daysContents[index].style.height="100%";
-    daysContents[index].style.borderColor="#5ffcad";
+    daysContents[index].style.borderColor="#5ffce7";
     daysNums[index].style.backgroundColor="#2d27fb60";
-    daysNums[index].style.borderColor="#5ffcad";
+    daysNums[index].style.borderColor="#5ffce7";
 }
 function offDay(index)
 {
@@ -25,26 +25,20 @@ function offDay(index)
 function onArticle(index)
 {
     var articleTitle = document.querySelectorAll("article > .title");
-    var articleContent = document.querySelectorAll("article > .content");
-    // var articleTitleMask = document.querySelectorAll("article > .title > .mask");
-    // articleTitleMask[index].style.height = "70%";
-    articleTitle[index].style.color = "#5ffcadb4";
-    articleTitle[index].style.borderBottom = "2px solid #5ffcad";
-    articleContent[index].style.borderLeft = "2px solid #5ffcad";
-    articleContent[index].style.borderBottom = "2px solid #5ffcad";
-    // articleContent[index].style.boxShadow = "0px 0px 10px rgb(107, 150, 190)";
+    var titleBorder = document.querySelectorAll("article > .title > .flexBorder");
+    articleTitle[index].style.color = "#5ffce7b4";
+    articleTitle[index].style.borderBottom = "2px solid #5ffce7";
+    titleBorder[index].style.height = "20px";
+    titleBorder[index].style.borderRight = "2px solid #5ffce7";
 }
 function offArticle(index)
 {
     var articleTitle = document.querySelectorAll("article > .title");
-    var articleContent = document.querySelectorAll("article > .content");
-    // var articleTitleMask = document.querySelectorAll("article > .title > .mask");
-    // articleTitleMask[index].style.height = "100%";
+    var titleBorder = document.querySelectorAll("article > .title > .flexBorder");
     articleTitle[index].style.color = "white";
     articleTitle[index].style.borderBottom = "2px solid white";
-    articleContent[index].style.borderLeft = "2px solid white";
-    articleContent[index].style.borderBottom = "none";
-    // articleContent[index].style.boxShadow = "2px 5px 10px rgb(107, 150, 190)";
+    titleBorder[index].style.height = "0px";
+    titleBorder[index].style.borderRight = "2px solid white";
 }
 var count = 0;
 function nextOne()
@@ -53,10 +47,10 @@ function nextOne()
     if(count < 2)
     {
         count++;
-        workshop[count - 1].style.height = "0px";
+        // workshop[count - 1].style.height = "0px";
         workshop[count - 1].style.width = "0px";
         workshop[count - 1].style.opacity = "0";
-        workshop[count].style.height = "500px";
+        // workshop[count].style.height = "500px";
         workshop[count].style.width = "400px";
         workshop[count].style.opacity = "1";
     }
@@ -66,10 +60,10 @@ function preOne()
     var workshop = document.getElementsByClassName("workshop");
     if(count > 0)
     {
-        workshop[count - 1].style.height = "500px";
+        // workshop[count - 1].style.height = "500px";
         workshop[count - 1].style.width = "400px";
         workshop[count - 1].style.opacity = "1";
-        workshop[count].style.height = "0px";
+        // workshop[count].style.height = "0px";
         workshop[count].style.width = "0px";
         workshop[count].style.opacity = "0";
         count--;
